f707b976efcd3e937af11a16481c929965f19cbe
Erik van Zijst <erik.van.zijst@gmail.com>
2025-09-18 16:37:45 +0200

    Break out investments and returns in chart
    Vanguard-style.

--- gainly/lit/main.py
+++ gainly/lit/main.py
@@ -9,6 +9,7 @@ from gainly.lit.util import row_selector
 from gainly.portfolio import PortfolioPerformance
 from gainly.yahoo import YahooFinance
 
+
 conn = st.connection('transactions', type='sql', ttl=0)
 with conn.session as session:
     # Initialize database:
@@ -46,16 +47,29 @@ with st.popover('Options'):
 def get_eod_prices(symbol: str, date_from: date, date_to: date) -> pd.DataFrame:
     return YahooFinance().get_oed_prices(symbol, date_from, date_to)
 
-cols = st.columns(2)
-
 txns = conn.query('''SELECT * FROM TXN ORDER BY trade_date ASC''', ttl=0)
 txns['trade_date'] = pd.to_datetime(txns['trade_date'])
-selector = row_selector(txns, st_cols=cols, key='txns', df_cols=['symbol', 'broker'])
-txns = selector(txns)
-portfolio = PortfolioPerformance(txns, YahooFinance())
+txns = row_selector(txns, st_cols=st.columns(2), key='txns', df_cols=['symbol', 'broker'])(txns)
 
-with st.expander('Positions'):
-    st.dataframe(portfolio.positions().reset_index()[['symbol', 'position', 'value', 'pl']],
+metric = st.container()
+chart = st.container()
+pos = st.container()
+trades = st.container()
+
+portfolio = PortfolioPerformance(txns, YahooFinance())
+valuations = portfolio.daily_valuations()
+positions = portfolio.positions().reset_index()
+pl = positions['pl'].sum()
+
+
+with metric:
+    st.metric('Portfolio value',
+              f"€{positions['value'].sum():,.2f}",
+              f"{pl / positions['invested'].sum() * 100:.2f}% | €{pl:,.2f}",
+              border=True, width='content')
+
+with pos, st.expander('Positions'):
+    st.dataframe(positions[['symbol', 'position', 'value', 'pl']],
                  use_container_width=False, hide_index=True,
                  column_config={
                      'position': st.column_config.NumberColumn(format='localized'),
@@ -63,15 +77,21 @@ with st.expander('Positions'):
                      'pl': st.column_config.NumberColumn(format='euro'),
                  })
 
-valuations = portfolio.daily_valuations()
-fig = px.area(valuations[['date', 'symbol', 'value']], x="date", y="value", color="symbol", line_group='symbol', title='Portfolio performance')
-for dt in valuations[valuations['price'] > 0]['date'].unique():
-    fig.add_vline(x=dt, line_dash="dot", line_color="black")
-fig.update_layout(
-    xaxis=dict(showgrid=True, tickformat="%d %b %Y", tickangle=45, title=''),
-    yaxis=dict(title='€', showticklabels=not st.session_state['hide_amounts'])
-)
-st.plotly_chart(fig, use_container_width=True)
+with chart:
+    df = (valuations[['date', 'invested', 'pl']]
+          .rename(columns={'pl': 'returns'})
+          .groupby('date')
+          .sum()
+          .reset_index()
+          .melt(id_vars=['date'], var_name='type', value_name='value'))
 
-with st.expander('Trade history'):
+    fig = px.area(df, x="date", y="value", color="type", line_group='type')
+    for dt in valuations[valuations['price'] > 0]['date'].unique():
+        fig.add_vline(x=dt, line_dash="dot", line_color="black")
+    fig.update_layout(
+        xaxis=dict(showgrid=True, tickformat="%d %b %Y", tickangle=45, title=''),
+        yaxis=dict(title='€', showticklabels=not st.session_state['hide_amounts']))
+    st.plotly_chart(fig, use_container_width=True)
+
+with trades, st.expander('Trade history'):
     st.dataframe(txns, use_container_width=True)
